dc636ebd9cfbcd3cc4ae9723f3fac5e72c2dd3ba
jhumer-apus <jhumerapus@gmail.com>
2024-09-13 22:19:03 +0800

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import AudioPlay from "@/components/AudioPlay";
 import BirthdayCountdown from "@/components/BirthdayCountdown";
 import ParticleComponent from "@/components/ParticleComponent";
-import Question from "@/components/Question";
 import Question1 from "@/components/Question1";
 import Question2 from "@/components/Question2";
 import Question3 from "@/components/Question3";
@@ -12,8 +11,6 @@ import Question5 from "@/components/Question5";
 import Question6 from "@/components/Question6";
 import Question7 from "@/components/Question7";
 import Question8 from "@/components/Question8";
-import { expression } from "@/data/expression";
-import Image from "next/image";
 import { Fragment, useEffect, useRef, useState } from "react";
 // import palagi from "@/public/assets/musics/palagi.mp3"
 
